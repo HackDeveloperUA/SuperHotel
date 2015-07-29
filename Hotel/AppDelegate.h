b636692082e64f70edc02d8973c7157b2f3fe7b7
MD <md@MacBook-Air-MD.Dlink>
2015-07-29 13:32:49 +0300

    Initial Commit

---
+++ Hotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hotel
+//
+//  Created by MD on 29.07.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hotel
+//
+//  Created by MD on 29.07.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
